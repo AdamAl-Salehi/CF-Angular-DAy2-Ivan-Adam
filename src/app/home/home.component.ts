5615d8d7e221fd14405137bf7c9ccd674edd3fe5
Ivan Zivanovic <i.zivanovic01@gmail.com>
2020-10-20 14:08:18 +0200

    02

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-
+  car = [
+    {
+      name: "Ford",
+      img: "/../../assets/Ford.png",
+    },{
+      name: "Audi",
+      img: "https://d1og9yuwrvksj4.cloudfront.net/at/A/library/A5/A5%402x_sm.png",
+    },{
+      name:"BMW",
+      img: "https://mediapool.bmwgroup.com/cache/P9/202005/P90389688/P90389688-the-bmw-7-series-05-2020-2000px.jpg",
+    }
+  ];
+ 
 
  constructor() { }
 
