3c4ebabd158254254899d59aeb3e606bb212e99c
AdamAl-Salehi <email@example.combakr.arkan@icloud.com>
2020-10-20 15:04:53 +0200

    adds form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
 
 @Component({
-  selector: 'contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+ selector: 'contact-home',
+ templateUrl: './contact.component.html',
+ styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+        clickCounter :number = 0;
+ 
+ info = new FormGroup({
+   firstName: new FormControl('',Validators.required),
+   lastName: new FormControl('',Validators.required),
+   age: new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$"),Validators.maxLength(2),]),
+   email:new FormControl('',[Validators.required,Validators.email]),
+   address:new FormControl('',Validators.required),
+ });
 
-  constructor() { }
 
-  ngOnInit(): void {
+ constructor() { }
+
+ ngOnInit() {
+ }
+
+ onSubmit(){
+  if(this.info.valid){
+    var a = this.info.value;
+    console.log(a)
+ }
   }
-
 }
